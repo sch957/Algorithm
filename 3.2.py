198465ed53b5708fd96d0e43cd7a5389642370f1
sch957 <162536148+sch957@users.noreply.github.com>
2024-04-25 13:20:49 +0900

    Delete 3.2.py

--- 3.2.py
+++
@@ -1,9 +0,0 @@
-def selection_sort(A,key):
-    for i in range(len(A)):
-        if A[i] == key:
-            return i
-    return -1    
-data = [5,3,8,4,9,1,6,2,7]
-print("Original : ", data)
-print(selection_sort(data,7))
-print("Selection : ", data)
